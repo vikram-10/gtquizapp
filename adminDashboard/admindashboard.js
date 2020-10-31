9ea07bceb6b82229cf3f766c12b0dfa3ba15921a
vikithespider@gmail.com <vikrambalaji2k@gmail.com>
2020-10-31 18:35:29 +0530

    Added updates

--- adminDashboard/admindashboard.js
+++ adminDashboard/admindashboard.js
@@ -1,6 +1,6 @@
 let navbar=document.createElement('nav');
 navbar.setAttribute('class','navbar navbar-expand-lg navbar-light bg-light');
-navbar.innerHTML=`    <a class="navbar-brand" href="index.html">The Guindy Times</a>
+navbar.innerHTML=`    <a class="navbar-brand" href="../index.html">The Guindy Times</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
@@ -8,7 +8,7 @@ navbar.innerHTML=`    <a class="navbar-brand" href="index.html">The Guindy Times
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
     </li>
   </ul>
 </div>`
